be6e2cb03254d403202fbb3e7a925af0a3fd59e7
gabeweng <gabeweng@gmail.com>
2024-01-21 11:08:12 -0500

    Add meal chain and import more libraries

--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
 from langchain.prompts import PromptTemplate
 import streamlit as st
+from langchain.llms import OpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
 
 API_KEY = st.secrets["openai_api_key"]
 
@@ -8,4 +11,15 @@ API_KEY = st.secrets["openai_api_key"]
 meal_template = PromptTemplate(
     input_variables=["ingredients"],
     template="Give me an example of 2 meals that could be made using the following ingredients: {ingredients}",
+)
+
+#chains
+
+llm = OpenAI(openai_api_key=API_KEY, temperature=0.9)
+
+meal_chain = LLMChain(
+    llm=llm,
+    prompt=meal_template,
+    output_key="meals",  # the output from this chain will be called 'meals'
+    verbose=True
 )
