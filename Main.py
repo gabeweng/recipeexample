fc802ed24680e22ee92a8138589de4e64e9fa19e
gabeweng <gabeweng@gmail.com>
2024-01-21 11:19:25 -0500

    More chains and templates

--- Main.py
+++ Main.py
@@ -13,6 +13,17 @@ meal_template = PromptTemplate(
     template="Give me an example of 2 meals that could be made using the following ingredients: {ingredients}",
 )
 
+gangster_template = """Re-write the meals given below in the style of a New York mafia gangster:
+
+Meals:
+{meals}
+"""
+
+gangster_template = PromptTemplate(
+    input_variables=['meals'],
+    template=gangster_template
+)
+
 #chains
 
 llm = OpenAI(openai_api_key=API_KEY, temperature=0.9)
@@ -21,5 +32,20 @@ meal_chain = LLMChain(
     llm=llm,
     prompt=meal_template,
     output_key="meals",  # the output from this chain will be called 'meals'
+    verbose=True
+)
+
+
+gangster_chain = LLMChain(
+    llm=llm,
+    prompt=gangster_template,
+    output_key="gangster_meals",  # the output from this chain will be called 'gangster_meals'
+    verbose=True
+)
+
+overall_chain = SequentialChain(
+    chains=[meal_chain, gangster_chain],
+    input_variables=["ingredients"],
+    output_variables=["meals", "gangster_meals"],
     verbose=True
 )
