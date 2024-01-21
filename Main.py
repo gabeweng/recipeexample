e8bf01a394014079255c2b7080f706dfe36255ea
gabeweng <gabeweng@gmail.com>
2024-01-21 11:05:26 -0500

    Add meal template

--- Main.py
+++ Main.py
@@ -2,3 +2,10 @@ from langchain.prompts import PromptTemplate
 import streamlit as st
 
 API_KEY = st.secrets["openai_api_key"]
+
+#templates
+
+meal_template = PromptTemplate(
+    input_variables=["ingredients"],
+    template="Give me an example of 2 meals that could be made using the following ingredients: {ingredients}",
+)
